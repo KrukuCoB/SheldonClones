adfd89823c711fa73cbd46cc83ce374f8a4d83a7
KrukuCoB <98872264+KrukuCoB@users.noreply.github.com>
2025-07-25 23:14:25 +0300

    Create CompProperties_NeighborAgreement.cs

---
+++ Source/CompProperties/CompProperties_NeighborAgreement.cs
@@ -0,0 +1,12 @@
+using Verse;
+
+namespace SheldonClones
+{
+    public class CompProperties_NeighborAgreement : CompProperties
+    {
+        public CompProperties_NeighborAgreement()
+        {
+            compClass = typeof(CompNeighborAgreement);
+        }
+    }
+}
